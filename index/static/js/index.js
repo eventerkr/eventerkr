585bbec0a4c85ae17cca55931b358c58bd3b349e
birdpocket <birdpocket@gmail.com>
2018-04-23 00:09:58 +0900

    minimal-implement event status button

--- index/static/js/index.js
+++ index/static/js/index.js
@@ -6,11 +6,45 @@ $(function(){
 	$("#event1_button").click(function(){
 		var table = document.getElementById("event_table");
 		var lst = document.getElementsByTagName("tr");
-        var eventstatusid = lst.getElementById("eventstatusid");
-        console.log(eventstatusid);
+        console.log(lst);
 		for (var i=0; i<lst.length; i++) {
-            if (lst[i].getElementById("eventstatusid") == "1") {
-                lst[i].style.display = 'none';
+            var cols = lst[i].getElementsByTagName('th');
+            for (var j=0; j<cols.length; j++) {
+                col = cols[j];
+                if (col.id == "eventstatusid") {
+                    console.log(col.id);
+                    console.log(col.innerText);
+                    if (col.innerText == '1') {
+                        lst[i].style.display = '';
+                    } else {
+                        lst[i].style.display = 'none';
+                    }
+                }
+            }
+        }
+		lst.border=1;
+    	return false;
+	});
+});
+
+$(function(){
+	$("#event2_button").click(function(){
+		var table = document.getElementById("event_table");
+		var lst = document.getElementsByTagName("tr");
+        console.log(lst);
+		for (var i=0; i<lst.length; i++) {
+            var cols = lst[i].getElementsByTagName('th');
+            for (var j=0; j<cols.length; j++) {
+                col = cols[j];
+                if (col.id == "eventstatusid") {
+                    console.log(col.id);
+                    console.log(col.innerText);
+                    if (col.innerText == '2') {
+                        lst[i].style.display = '';
+                    } else {
+                        lst[i].style.display = 'none';
+                    }
+                }
             }
         }
 		lst.border=1;
