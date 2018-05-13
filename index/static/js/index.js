76179dc7525ef8fb9c23ce099255e99d32acc2e0
birdpocket <birdpocket@gmail.com>
2018-05-14 01:54:28 +0900

    implements status3 button

--- index/static/js/index.js
+++ index/static/js/index.js
@@ -51,3 +51,28 @@ $(function(){
     	return false;
 	});
 });
+
+$(function(){
+	$("#event3_button").click(function(){
+		var table = document.getElementById("event_table");
+		var lst = document.getElementsByTagName("tr");
+        console.log(lst);
+		for (var i=0; i<lst.length; i++) {
+            var cols = lst[i].getElementsByTagName('th');
+            for (var j=0; j<cols.length; j++) {
+                col = cols[j];
+                if (col.id == "eventstatusid") {
+                    console.log(col.id);
+                    console.log(col.innerText);
+                    if (col.innerText == '3') {
+                        lst[i].style.display = '';
+                    } else {
+                        lst[i].style.display = 'none';
+                    }
+                }
+            }
+        }
+		lst.border=1;
+        return false;
+	});
+});
